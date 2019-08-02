e39bd143816a634008e9454be9482e1d93400fe0
Seniru Pasan Indira <senirupasan@gmail.com>
2019-08-02 11:40:27 +0530

    Added a new feature "Learn"
    Mice can select any course they like according to their educational level and stream and complete it (Those educational qualifications can be used in future to have a job they like). 
    
    Testers needed!!!

--- game.lua
+++ game.lua
@@ -13,6 +13,7 @@ local CONSTANTS = {
 
 local players = {}
 local healthPacks = {}
+local courses = {}
 
 --creating the class Player
 
@@ -38,6 +39,11 @@ function Player.new(name)
     self.healthRate = 0.002
     self.xp = 0
     self.level = 1
+    self.learning = ""
+    self.learnProgress = 0
+    self.eduLvl = 1
+    self.eduStream = ""
+    self.degrees = {}
     ui.addTextArea(self.healthBarId, "", name, CONSTANTS.BAR_X, 340, CONSTANTS.BAR_WIDTH, 20, 0xff0000, 0xee0000, 1, true)
     ui.addTextArea(self.xpBarId, "", name, CONSTANTS.BAR_X, 370, 1, 17, 0x00ff00, 0x00ee00, 1, true)
     return self
@@ -50,6 +56,11 @@ function Player:getHealthBarId() return self.healthBarId end
 function Player:getHealthRate() return self.healthRate end
 function Player:getXP() return self.xp end
 function Player:getLevel() return self.level end
+function Player:getLearningCourse() return self.learning end
+function Player:getLearningProgress() return self.learnProgress end
+function Player:getEducationLevel() return self.eduLvl end
+function Player:getEducationStream() return self.eduStream end
+function Player:getDegrees() return self.degrees end
 
 function Player:work()
     if self.health -0.05 > 0 then
@@ -89,7 +100,35 @@ function Player:setXP(val, add)
     end
     ui.addTextArea(self.xpBarId, "", self.name, CONSTANTS.BAR_X, 370, ((self.xp - calculateXP(self.level)) / (calculateXP(self.level + 1) - calculateXP(self.level)))  * CONSTANTS.BAR_WIDTH, 17, 0x00ff00, 0x00ee00, 1, false)
     ui.updateTextArea(3, "<p align='center'>Level " .. self.level .. " - " ..self.xp .. "/" .. calculateXP(self.level + 1) .. "XP", self.name)
+end
 
+function Player:setCourse(course)
+  self.learning = course.name
+  self.learnProgress = 0
+  self.eduLvl = course.level
+  self.eduStream = course.stream
+end
+
+function Player:addDegree(course)
+  table.insert(self.degrees, course)
+end
+
+function Player:learn()
+  if learning == "" then
+    print("No course!")
+  else
+print(self.learning)
+    if self.money > courses[self.learning].feePerLesson then
+      self.learnProgress = self.learnProgress + 1
+      self:setMoney(-courses[self.learning].feePerLesson, true)
+      if self.learnProgress >= courses[self.learning].lessons then
+        self:addDegree(self.learning)
+        print("Graduated")
+        self.learning = ""
+        self.eduLvl = self.eduLvl + 1
+      end
+    end
+  end
 end
 
 function Player:levelUp()
@@ -119,6 +158,17 @@ function displayShop(target)
     ui.addTextArea(100, "<p align='center'><font size='20'><b><J>Shop</J></b></font></p><br></br>" .. medicTxt, target, 200, 90, 400, 200, nil, nil, 1, true)
 end
 
+function displayCourses(target)
+  local courseTxt = ""
+  local p = players[target]
+  for id, course in pairs(courses) do
+    if p:getEducationLevel() == course.level and (p:getEducationStream() == course.stream or p:getEducationStream() == "") and learning ~= "" then
+      courseTxt = courseTxt .. id .. " Fee: " .. course.fee .. " Lessons: " .. course.lessons .. " <a href='event:" .. course.uid .. "'>Enroll</a>'<br>"
+    end
+  end
+  ui.addTextArea(200, "<p align='center'><font size='20'><b><J>Courses</J></b></font></p><br></br>" .. courseTxt, target, 200, 90, 400, 200, nil, nil, 1, true)
+end
+
 function calculateXP(lvl)
     return 2.5 * (lvl + 2) * (lvl - 1)
 end
@@ -134,6 +184,18 @@ function HealthPack(_name, _price, _regainVal, _adding, _desc)
   }
 end
 
+function Course(_name, _fee, _lessons, _level, _stream)
+  courses[_name] = {
+    name = _name,
+    fee = _fee,
+    lessons = _lessons,
+    level = _level,
+    stream = _stream,
+    feePerLesson = _fee / _lessons,
+    uid = "course:" .. _name 
+  }
+end
+
 function setUI(name)
     --textAreas
     --work
@@ -145,7 +207,9 @@ function setUI(name)
     --xp bar area
     ui.addTextArea(3, "<p align='center'>Level 1  -  0/" .. calculateXP(2) .. "XP</p>", name, CONSTANTS.BAR_X, 370, CONSTANTS.BAR_WIDTH, 20, nil, nil, 0.5, true)
     --shop button
-    ui.addTextArea(40, "<a href='event:shop'>Shop</a>", name, 740, 300, 36, 20, nil, nil, 1, true)
+    ui.addTextArea(4, "<a href='event:shop'>Shop</a>", name, 740, 300, 36, 20, nil, nil, 1, true)
+    --school button
+    ui.addTextArea(5, "<a href='event:courses'>Learn</a>", name, 740, 270, 36, 20, nil, nil, 1, true)
 end
 
 --event handling
@@ -169,10 +233,19 @@ function eventTextAreaCallback(id, name, evt)
         players[name]:work()
     elseif evt == "shop" then
         displayShop(name)
+    elseif evt == "courses" then
+        if players[name]:getLearningCourse() == "" then
+          displayCourses(name)
+        else 
+          players[name]:learn()
+        end
     elseif string.sub(evt, 1, 6) == "health" and players[name]:getMoney() - healthPacks[string.sub(evt, 8)].price >= 0 then
         local pack = healthPacks[string.sub(evt, 8)]
         players[name]:useMed(pack)
         players[name]:setMoney(-pack.price, true)
+    elseif string.sub(evt, 1, 6) == "course" then
+        players[name]:setCourse(courses[string.sub(evt, 8)])
+        ui.removeTextArea(id, name)    
     end
 end
 
@@ -186,8 +259,18 @@ end
 
 --game logic
 
+--creating and storing HealthPack tables
 HealthPack("Cheese", 5, 0.01, true,  "Just a cheese! to refresh yourself")
 HealthPack("Cheese Pizza", 30, 0.05, true, "dsjfsdlkgjsdk")
+--creating and storing Course tables
+Course("School", 20, 2, 1, "")
+Course("Junior miner", 10, 4, 1, "")
+Course("High School", 500, 20, 2, "")
+Course("Cheese miner", 1000, 30, 3, "admin")
+Course("Cheese trader", 2500, 30, 3, "bs")
+Course("Cheese developer", 2500, 50, 3, "it")
+Course("Cheese trader-II", 90000, 75, 4, "bs")
+Course("Fullstack cheese developer", 10000, 70, 4, "it")
 
 for name, player in pairs(tfm.get.room.playerList) do
     players[name] = Player(name)
